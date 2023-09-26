c2d97a4e68b28c38e28ba87c7285b473fb16ac6f
nurudeensulaimon <nurudeen.a.sulaimon@gmail.com>
2023-09-26 11:33:44 -0400

    Add Class Attendee

--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -4,13 +4,15 @@ require './lib/item'
 RSpec.describe Item do 
   before(:each) do 
     @item1 = Item.new('Chalkware Piggy Bank')
+    @item2 = Item.new('Bamboo Picture Frame')
   end
    
    it 'exists' do
     expect(@item1).to be_a_instance_of(Item)
+    expect(@item2).to be_a_instance_of(Item)
    end
 
    it 'has attributes' do 
-    expect(@item1.name).to eq("Chalkware Piggy Bank" a)
+    expect(@item1.name).to eq("Chalkware Piggy Bank")
    end
 end
